acc30dfdfbcec071ad125dc708eb674b53dfdb8a
Tesssssssssy <ewoo97@naver.com>
2024-05-07 15:59:42 +0900

    [Solve] 프로그래머스 해시 문제 <폰켓몬> 풀이 완료

---
+++ src/main/java/week1/lim/Hash.java
@@ -0,0 +1,67 @@
+package week1.lim;
+
+public class Hash {
+    // Hash table
+    public Slot[] hashTable; // 배열 형태로 선언
+
+    // Hash 객체를 생성할 때 table 사이즈 지정
+    public Hash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    // Slot에는 value를 가짐
+    public class Slot {
+        String value;
+
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+
+    // 해시 함수
+    public int hashFunction(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length; // 나머지
+    }
+
+    // 입력 받은 key를 해시 함수로 인덱스화 하고, 해당 인덱스에 value 저장
+    public boolean saveData(String key, String value) {
+        // key는 해시 함수를 거쳐서 해시 값(해시, 해시 주소)을 반환 -> 여기선 배열의 index와 동일
+        Integer address = this.hashFunction(key);
+
+        System.out.println(address);
+
+        if (this.hashTable[address] != null) { // 해당 주소에 이미 데이터가 있을 경우
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+
+        return true;
+    }
+
+    // key에 해당하는 값을 반환
+    public String getData(String key) {
+        // key는 해시 함수를 거쳐서 해시 값(해시, 해시 주소)을 반환
+        Integer address = this.hashFunction(key);
+
+        if(this.hashTable[address] != null) {
+            return this.hashTable[address].value;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Hash myHash = new Hash(20);
+
+        myHash.saveData("Lim","30000");     // address: 16
+        myHash.saveData("Hong","15000");    // address: 12
+        myHash.saveData("Kim","5000");      // address: 15
+        myHash.saveData("Baek","5000");     // address: 6
+
+        System.out.println(myHash.getData("Lim"));      // 30000
+        System.out.println(myHash.getData("Hong"));     // 15000
+        System.out.println(myHash.getData("Kim"));      // 5000
+        System.out.println(myHash.getData("Baek"));     // 5000
+    }
+}
---
+++ src/main/java/week1/lim/HashMapEx.java
@@ -0,0 +1,54 @@
+package week1.lim;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+    public static void main(String[] args) {
+        HashMap<String, String> h1 = new HashMap<String, String>();
+        HashMap<String, String> h2 = new HashMap<String, String>();
+
+        h1.put("aaa", "1111");
+        h1.put("bbb", "2222");
+        h1.put("ccc", "3333");
+        h1.putIfAbsent("aaa", "0000");
+        h1.putIfAbsent("ddd", "4444");
+        h2.putAll(h1);
+        System.out.println("h1 : " + h1);
+        System.out.println("h2 : " + h2);
+
+        System.out.println("[1]: " + h1.containsKey("aaa"));
+        System.out.println("[2]: " + h1.containsValue("1111"));
+        System.out.println("[3]: " + h1.isEmpty());
+        System.out.println("[4]: " + h1.size());
+        System.out.println("[5]: " + h1);
+        System.out.println("[6]: " + h1.remove("aaa", "1111"));
+        System.out.println("[7]: " + h1.put("bbb", "0000"));
+        System.out.println("[8]: " + h1.replace("ccc", "0000"));
+        System.out.println("h1 : " + h1);
+        System.out.println("h2 : " + h2);
+
+
+        for (String key: h1.keySet()) {
+            String value = h1.get(key);
+            System.out.println("Key:" + key + ", Value:" + value);
+        }
+    }
+
+    /*
+        h1 : {aaa=1111, ccc=3333, bbb=2222, ddd=4444}
+        h2 : {aaa=1111, ccc=3333, bbb=2222, ddd=4444}
+        [1]: true
+        [2]: true
+        [3]: false
+        [4]: 4
+        [5]: {aaa=1111, ccc=3333, bbb=2222, ddd=4444}
+        [6]: true
+        [7]: 2222
+        [8]: 3333
+        h1 : {ccc=0000, bbb=0000, ddd=4444}
+        h2 : {aaa=1111, ccc=3333, bbb=2222, ddd=4444}
+        Key:ccc, Value:0000
+        Key:bbb, Value:0000
+        Key:ddd, Value:4444
+    */
+}
