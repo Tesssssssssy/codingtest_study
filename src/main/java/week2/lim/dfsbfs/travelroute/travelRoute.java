0d23ae1ce5d7d3b5b685cc0c6bc0f03fe2e2cf1d
Tesssssssssy <ewoo97@naver.com>
2024-05-19 20:56:47 +0900

    [Solve] [프로그래머스] 깊이/너비 우선 탐색 - 여행 경로 문제 풀이 완료

---
+++ src/main/java/week2/lim/dfsbfs/travelroute/travelRoute.java
@@ -0,0 +1,42 @@
+package week2.lim.dfsbfs.travelroute;
+
+// 문제: https://school.programmers.co.kr/learn/courses/30/lessons/43164
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class travelRoute {
+    static ArrayList<String> list = new ArrayList<>();
+    static boolean useTickets[];
+
+    public static String[] solution(String[][] tickets) {
+        useTickets = new boolean[tickets.length];
+
+        dfs(0, "ICN", "ICN", tickets);
+
+        Collections.sort(list);
+
+        return list.get(0).split(" ");
+    }
+
+    static void dfs(int depth, String now, String path, String[][] tickets){
+        if (depth == tickets.length) {
+            list.add(path);
+            return;
+        }
+
+        for (int i = 0; i < useTickets.length; i++) {
+            if (!useTickets[i] && now.equals(tickets[i][0])) {
+                useTickets[i] = true;
+                dfs(depth+1, tickets[i][1], path + " " +tickets[i][1], tickets);
+                useTickets[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+        System.out.println(Arrays.toString(solution(tickets))); // [ICN, JFK, HND, IAD]
+    }
+}
