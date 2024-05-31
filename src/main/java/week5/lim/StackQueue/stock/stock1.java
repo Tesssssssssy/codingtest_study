254e3ade14a29bc68c86d584c8d472feea5d3266
Tesssssssssy <ewoo97@naver.com>
2024-05-31 09:47:37 +0900

    [Solve] 프로그래머스 스택&큐 - <주식가격> 문제 풀이 완료

---
+++ src/main/java/week5/lim/StackQueue/stock/stock1.java
@@ -0,0 +1,30 @@
+package week5.lim.StackQueue.stock;
+
+// 문제: https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+import java.util.Arrays;
+
+public class stock1 {
+    /**
+     *  stack/queue 사용하지 않고 이중 for문을 사용한 방법
+     */
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++; // 뒤에 있는 값들 보다 작거나 같을 때 인덱스 값 하나씩 추가
+
+                if (prices[i] > prices[j]) { // 크면 다음 인데스 비교
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        System.out.print(Arrays.toString(solution(prices)));  // [4, 3, 1, 1, 0]
+    }
+}
