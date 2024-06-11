7bfccbc8ae2e90c8ff9dd9d64f100e5e523b9f5a
Tesssssssssy <ewoo97@naver.com>
2024-06-11 11:07:58 +0900

    [Solve] 프로그래머스 힙 - <이중우선순위큐> 문제 풀이 완료

---
+++ src/main/java/week6/lim/heap/dualpriorityqueue/DualPriorityQueue1.java
@@ -0,0 +1,90 @@
+package week6.lim.heap.dualpriorityqueue;
+
+// 문제: https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+import java.util.*;
+
+public class DualPriorityQueue1 {
+    public static int[] solution(String[] operations) {
+        // 최솟값을 관리할 최소 힙
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // 최댓값을 관리할 최대 힙
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        // 각 숫자의 힙에 존재하는 개수를 추적하기 위한 맵
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        // operations 배열을 순회하면서 각 연산을 수행
+        for (String op : operations) {
+            String[] parts = op.split(" ");
+            String command = parts[0];
+            int number = Integer.parseInt(parts[1]);
+
+            if (command.equals("I")) {
+                // 'I' 명령어 처리: 숫자 삽입
+                // 두 힙에 숫자를 추가하고, countMap에 해당 숫자의 개수를 증가
+                minHeap.offer(number);
+                maxHeap.offer(number);
+                countMap.put(number, countMap.getOrDefault(number, 0) + 1);
+            } else if (command.equals("D")) {
+                if (number == 1) {
+                    // 'D 1' 명령어 처리: 최댓값 삭제
+                    // 최대 힙에서 유효한 최댓값을 찾아 제거
+                    remove(maxHeap, countMap);
+                } else if (number == -1) {
+                    // 'D -1' 명령어 처리: 최솟값 삭제
+                    // 최소 힙에서 유효한 최솟값을 찾아 제거
+                    remove(minHeap, countMap);
+                }
+            }
+        }
+
+        // 모든 연산 후 최댓값과 최솟값을 추출
+        int max = extractValid(maxHeap, countMap);
+        int min = extractValid(minHeap, countMap);
+
+        // 큐가 비어있으면 [0, 0] 반환, 그렇지 않으면 [최댓값, 최솟값] 반환
+        if (max == -1 || min == -1) {
+            return new int[]{0, 0};
+        } else {
+            return new int[]{max, min};
+        }
+    }
+
+    private static void remove(PriorityQueue<Integer> heap, Map<Integer, Integer> countMap) {
+        // 주어진 힙에서 유효한 값(실제로 존재하는 값)을 찾아 제거
+        while (!heap.isEmpty()) {
+            int num = heap.poll();
+            int count = countMap.getOrDefault(num, 0);
+            if (count > 1) {
+                // 해당 숫자가 여러 개 있으면 개수를 하나 줄임
+                countMap.put(num, count - 1);
+                break;
+            } else if (count == 1) {
+                // 해당 숫자가 하나만 있으면 맵에서 완전히 제거
+                countMap.remove(num);
+                break;
+            }
+            // 만약 count가 0이면 이미 제거된 것으로 간주하고 계속 다음을 찾음
+        }
+    }
+
+    private static int extractValid(PriorityQueue<Integer> heap, Map<Integer, Integer> countMap) {
+        // 주어진 힙에서 유효한 최대값 또는 최솟값을 추출
+        while (!heap.isEmpty()) {
+            int num = heap.peek();
+            if (countMap.containsKey(num)) {
+                return heap.poll(); // 유효한 값이면 반환
+            } else {
+                heap.poll(); // 유효하지 않은 값(이미 제거된 값)은 그냥 제거
+            }
+        }
+        return -1; // 힙이 비어있으면 -1 반환
+    }
+
+    public static void main(String[] args) {
+        String[] operations1 = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        String[] operations2 = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        System.out.println(Arrays.toString(solution(operations1))); // [0, 0]
+        System.out.println(Arrays.toString(solution(operations2))); // [333, -45]
+    }
+}
---
+++ src/main/java/week6/lim/heap/dualpriorityqueue/DualPriorityQueue2.java
@@ -0,0 +1,59 @@
+package week6.lim.heap.dualpriorityqueue;
+
+// 문제: https://school.programmers.co.kr/learn/courses/30/lessons/42628
+// 참고: https://velog.io/@zayson/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Java-%EC%9D%B4%EC%A4%91-%EC%9A%B0%EC%84%A0%EC%88%9C%EC%9C%84-%ED%81%90
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class DualPriorityQueue2 {
+    public static int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        //최소 힙, 최대 힙
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String op : operations) {
+            StringTokenizer st = new StringTokenizer(op);
+            String judge = st.nextToken();
+            int value = Integer.parseInt(st.nextToken());
+
+            // 빈 큐에 데이터를 삭제 요청 경우 연산 무시
+            if (pq.size() < 1 && judge.equals("D"))
+                continue;
+
+            // 삽입 시 최소 힙, 최대 힙에 value 넣기
+            if (judge.equals("I")) {
+                pq.offer(value);
+                maxPq.offer(value);
+                continue;
+            }
+
+            // 나머지 경우는 D이면서 value값이 1인지 -1인지 이므로
+            // 0보다 작은 경우 최소 힙에서 poll후 최대힙에서 해당 원소 삭제
+            if (value < 0) {
+                int min = pq.poll();
+                maxPq.remove(min);
+                continue;
+            }
+
+            // 최대 힙에서 poll후 최소힙에서 해당 원소 삭제
+            int max = maxPq.poll();
+            pq.remove(max);
+        }
+        if (pq.size() > 0) {
+            answer[0] = maxPq.poll();
+            answer[1] = pq.poll();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] operations1 = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        String[] operations2 = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        System.out.println(Arrays.toString(solution(operations1))); // [0, 0]
+        System.out.println(Arrays.toString(solution(operations2))); // [333, -45]
+    }
+}
