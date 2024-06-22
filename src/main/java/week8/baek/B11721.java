d402020175a32f32c4be75c0ba0b2bd32545b374
SongYeon <songyeon0607@naver.com>
2024-06-22 16:45:28 +0900

    [Solve] 8주차 백준 #11721 업로드

---
+++ src/main/java/week8/baek/B11721.java
@@ -0,0 +1,17 @@
+package week8.baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i+=10) {
+            if(i+10 < input.length()) System.out.println(input.substring(i, i + 10));
+            else System.out.println(input.substring(i));
+        }
+    }
+}
