d785bbc334f764c906bb75047ffe293fa27c31c9
SongYeon <songyeon0607@naver.com>
2024-06-24 16:43:47 +0900

    [Solve] 8주차 백준 #2460 업로드

---
+++ src/main/java/week8/baek/B2460.java
@@ -0,0 +1,26 @@
+package week8.baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2460 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] on = new int[10];
+
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            String input = br.readLine();
+            count -= Integer.parseInt(input.split(" ")[0]);
+            count  += Integer.parseInt(input.split(" ")[1]);
+            on[i] = count;
+        }
+
+        Arrays.sort(on);
+
+        System.out.println(on[on.length-1]);
+    }
+}
