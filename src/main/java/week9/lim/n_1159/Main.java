ae07f6c24d6cc7764e58a043eb2369dffc5d1b73
Tesssssssssy <ewoo97@naver.com>
2024-07-01 11:06:42 +0900

    [Solve] [백준 - 브론즈] 유레카 이론 문제 풀이 완료 (#32)
    🖋농구 경기
    https://www.acmicpc.net/problem/1159

---
+++ src/main/java/week9/lim/n_1159/Main.java
@@ -0,0 +1,38 @@
+package week9.lim.n_1159;
+
+// 문제: https://www.acmicpc.net/problem/1159
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 문자별 출현 횟수 저장
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String lastName = br.readLine();
+            char firstChar = lastName.charAt(0);
+            // 해당 문자의 출현 횟수 업데이트
+            charCount.put(firstChar, charCount.getOrDefault(firstChar, 0) + 1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        // HashMap의 키셋을 순회하면서 출현 횟수가 5 이상인지 확인
+        for (char c : charCount.keySet()) {
+            if (charCount.get(c) >= 5) { // 해당 문자의 출현 횟수가 5 이상인지 확인
+                result.append(c);
+            }
+        }
+
+        // 결과 출력
+        if (result.length() == 0) {
+            System.out.println("PREDAJA");
+        } else {
+            System.out.println(result);
+        }
+    }
+}
