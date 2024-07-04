f9653ccfbcc00334d3de9d350fa4fdd1b4b523eb
Tesssssssssy <ewoo97@naver.com>
2024-07-04 13:34:32 +0900

    [Solve] [백준 - 브론즈] ROT13 문제 풀이 완료 (#41)
    🖋 ROT13
    https://www.acmicpc.net/problem/11655

---
+++ src/main/java/week9/lim/week9_2/n_11655/Main.java
@@ -0,0 +1,27 @@
+package week9.lim.week9_2.n_11655;
+
+// 문제: https://www.acmicpc.net/problem/11655
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(rot13(input));
+    }
+
+    private static String rot13(String plaintext) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < plaintext.length(); i++) {
+            char c = plaintext.charAt(i);
+            if (c >= 'a' && c <= 'm' || c >= 'A' && c <= 'M') {
+                c += 13;
+            } else if (c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z') {
+                c -= 13;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
