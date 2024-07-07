6c51f1d4eb01f88844ac827447450534143fe84f
Tesssssssssy <ewoo97@naver.com>
2024-07-07 23:55:06 +0900

    [Solve] [백준 - 브론즈] "한조서열정리하고옴ㅋㅋ" 문제 풀이 완료 (#58)
    🖋 한조서열정리하고옴ㅋㅋ
    https://www.acmicpc.net/problem/14659

---
+++ src/main/java/week10/lim/week10_1/n_14659/Main.java
@@ -0,0 +1,42 @@
+package week10.lim.week10_1.n_14659;
+
+// 문제: https://www.acmicpc.net/problem/14659
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     *  1. N개의 봉우리.
+     *  2. 자신보다 낮은 봉우리에 서있는 적들만 처치 가능.
+     *  3. 처음 출발한 봉우리보다 높은 봉우리를 만나면 그대로 공격을 포기하고 낮잠.
+     *  4. 봉우리의 높이는 모두 다르고 모든 용들은 오른쪽으로만 감.
+     *  5. 활잡이 중 최고의 활잡이가 최대 몇 명의 적을 처리할 수 있는지 출력.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] mountaintop = new int[N + 1];
+
+        for (int i = 0; i < N; i++) {
+            mountaintop[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxKilled = 0;
+
+        for (int i = 0; i < N; i++) {
+            int killed = 0;
+            for (int j = i + 1; j < N; j++) {
+                if (mountaintop[i] > mountaintop[j]) {
+                    killed++;
+                } else {
+                    break;
+                }
+            }
+            maxKilled = Math.max(maxKilled, killed);
+        }
+
+        System.out.println(maxKilled);
+    }
+}
