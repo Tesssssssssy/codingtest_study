395841ad76cfa65358d6fb9b51c42eaa601b0186
Tesssssssssy <ewoo97@naver.com>
2024-07-23 05:05:46 +0900

    [Solve] BaekJoon Silver | [정수 제곱근] 문제 풀이 완료 (#140)
    🖋 정수 제곱근 > 실버4
    https://www.acmicpc.net/problem/2417

---
+++ src/main/java/week12/lim/week12_1/silver/n_2417/Main.java
@@ -0,0 +1,28 @@
+package week12.lim.week12_1.silver.n_2417;
+
+// 문제: https://www.acmicpc.net/problem/2417
+
+import java.io.*;
+
+public class Main {
+    /**
+     * 1. 정수가 주어지면, 정수 제곱근을 구하라.
+     *
+     * [입력]
+     * 정수 n
+     *
+     * [출력]
+     * q^2 >= n인 가장 작은 음이 아닌 정수 q를 출력
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        long q = (long) Math.sqrt(n);
+
+        if ((q * q) < n)
+            q++;
+
+        System.out.println(q);
+    }
+}
---
+++ src/main/java/week12/lim/week12_1/silver/n_2417/Main2.java
@@ -0,0 +1,40 @@
+package week12.lim.week12_1.silver.n_2417;
+
+// 문제: https://www.acmicpc.net/problem/2417
+
+import java.io.*;
+
+public class Main2 {
+    /**
+     * 1. 정수가 주어지면, 정수 제곱근을 구하라.
+     *
+     * [입력]
+     * 정수 n
+     *
+     * [출력]
+     * q^2 >= n인 가장 작은 음이 아닌 정수 q를 출력
+     */
+    static long n, answer = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Long.parseLong(br.readLine());
+        binarySearch(n);
+        System.out.println(answer);
+    }
+
+    static void binarySearch(long key) {
+        long left = 0;
+        long right = key;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (key <= Math.pow(mid, 2)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+}
