cd82872b118c15de7e23c9015bf309c1d6d8b930
Tesssssssssy <ewoo97@naver.com>
2024-08-13 08:04:17 +0900

    [Chore] BaseMain.java 추가

---
+++ src/main/java/week14/lim/BaseMain.java
@@ -0,0 +1,16 @@
+package week14.lim;
+
+// 문제:
+
+import java.io.*;
+import java.util.*;
+
+public class BaseMain {
+    /**
+     *
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
