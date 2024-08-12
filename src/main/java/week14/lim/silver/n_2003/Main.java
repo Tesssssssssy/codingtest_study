d88a7e44d6fe1b5d0a5ff0df02723937556e00b3
Tesssssssssy <ewoo97@naver.com>
2024-08-13 07:56:11 +0900

    [Solve] BaekJoon Silver | "수들의 합 (S Lv.4)" 문제 풀이 완료 (#212)
    🖋 수들의 합 (S Lv.4)
    https://www.acmicpc.net/problem/2003

---
+++ src/main/java/week14/lim/silver/n_2003/Main.java
@@ -0,0 +1,56 @@
+package week14.lim.silver.n_2003;
+
+// 문제: https://www.acmicpc.net/problem/2003
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     *    N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다.
+     *    이 수열의 i번째 수부터 j번째 수까지의 합 A[i] + A[i+1] + … + A[j-1] + A[j]가 M이 되는 경우의 수를 구하는 프로그램.
+     *
+     *    [입력]
+     *    첫째 줄에 N(1 ≤ N ≤ 10,000), M(1 ≤ M ≤ 300,000,000)이 주어진다.
+     *    다음 줄에는 A[1], A[2], …, A[N]이 공백으로 분리되어 주어진다.
+     *    ( 각각의 A[x]는 30,000을 넘지 않는 자연수이다. )
+     *
+     *    [출력]
+     *    첫째 줄에 경우의 수를 출력.
+     *
+     *    [풀이]
+     *    투 포인터(Two Pointer) 기법을 사용해 배열의 연속된 부분 수열의 합을 계산
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        int sum = 0;
+        int start = 0;
+
+        for (int end = 0; end < N; end++) {
+            sum += arr[end];
+
+            while (sum > M) {
+                // sum이 M을 초과하면, start 포인터를 오른쪽으로 이동하면서 sum에서 해당 요소를 빼준다.
+                sum -= arr[start++];
+            }
+
+            if (sum == M) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
