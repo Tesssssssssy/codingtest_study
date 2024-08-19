f74917cd990b761fe8cd6ebf2c823f725c3af34e
Tesssssssssy <ewoo97@naver.com>
2024-08-19 17:30:26 +0900

    [Solve] Programmers Lv.2 - [땅따먹기] 문제 풀이 완료 (#236)
    🖋 땅따먹기 (Lv.2)
    https://school.programmers.co.kr/learn/courses/30/lessons/12913

---
+++ src/main/java/week15/lim/programmers_Lv2/n_12913/Solution.java
@@ -0,0 +1,45 @@
+package week15.lim.programmers_Lv2.n_12913;
+
+// 문제: https://school.programmers.co.kr/learn/courses/30/lessons/12913
+
+public class Solution {
+    /**
+     *  땅따먹기 게임을 하려고 합니다.
+     *  땅따먹기 게임의 땅(land)은 총 N행 4열로 이루어져 있고,
+     *  모든 칸에는 점수가 쓰여 있습니다.
+     *  1행부터 땅을 밟으며 한 행씩 내려올 때, 각 행의 4칸 중 한 칸만 밟으면서 내려와야 합니다.
+     *  단, 땅따먹기 게임에는 한 행씩 내려올 때, 같은 열을 연속해서 밟을 수 없는 특수 규칙이 있습니다.
+     *
+     *  예를 들면,
+     *  | 1 | 2 | 3 | 5 |
+     *  | 5 | 6 | 7 | 8 |
+     *  | 4 | 3 | 2 | 1 |   로 땅이 주어졌다면,
+     *  1행에서 네번째 칸 (5)를 밟았으면,
+     *  2행의 네번째 칸 (8)은 밟을 수 없습니다.
+     *
+     *  마지막 행까지 모두 내려왔을 때,
+     *  얻을 수 있는 점수의 최대값을 return
+     *
+     *  위 예의 경우, 1행의 네번째 칸 (5), 2행의 세번째 칸 (7), 3행의 첫번째 칸 (4) 땅을 밟아
+     *  16점이 최고점이 되므로 16을 return.
+     */
+    public static int solution(int[][] land) {
+        int n = land.length;
+
+        // DP 테이블을 업데이트하면서 최대 점수를 계산
+        for (int i = 1; i < n; i++) {
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+            land[i][2] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][3]);
+            land[i][3] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][2]);
+        }
+
+        // 마지막 행에서의 최댓값을 반환
+        return Math.max(Math.max(land[n-1][0], land[n-1][1]), Math.max(land[n-1][2], land[n-1][3]));
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+        System.out.println(solution(land)); // 16
+    }
+}
